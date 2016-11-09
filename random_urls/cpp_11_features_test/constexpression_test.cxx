f86b7c54ff3c70105cee7f51ad3d43c422f3d1fe
kmoza <krishoza15sep@gmail.com>
2016-11-09 12:25:41 +0530

    constexpr and initializer test

---
+++ random_urls/cpp_11_features_test/constexpression_test.cxx
@@ -0,0 +1,24 @@
+#include<iostream>
+
+constexpr int get_five()
+{
+	return 5;
+}
+
+int main()
+{
+	int some_array[get_five()+2]; //the array has lenght 7
+	for(int i = 0; i < 7; i++)
+	{
+		some_array[i] = i*10;
+	}
+
+	//display the array
+	for(int i = 0; i < 7; i++)
+	{
+		std::cout << some_array[i] << std::endl;
+	}
+
+	return 0;
+}
+	
---
+++ random_urls/cpp_11_features_test/initializer_list.cxx
@@ -0,0 +1,15 @@
+#include<iostream>
+
+struct Object
+{
+	float first;
+	int second;
+};
+
+int main()
+{
+	Object scalar = {0.43f, 10};
+	Object anArray[] = {{10.3f,20},{43.5f,30}};
+
+	return 0;
+}
