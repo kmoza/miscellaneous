ffc0c9edecf0deb53493ed8525224e7c1ce90a43
kmoza <krishoza15sep@gmail.com>
2016-11-14 17:58:35 +0530

    alloc and free

---
+++ random_urls/c_functions/mem_alloc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int *array = malloc(10*sizeof(int));
+
+	if(NULL == array)
+	{
+		fprintf(stderr,"malloc failed\n");
+		return -1;
+	}
+	return 0;
+}
