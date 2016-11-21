951b33340e6c68a2c01b1409b3005b77fcedb8cb
kmoza <krishoza15sep@gmail.com>
2016-11-21 14:56:56 +0530

    valgrind test

---
+++ random_urls/valgrind_tests/valgrind_test2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+char* lower;
+
+char* to_lower(const char* str)
+{
+	char* l = strdup(str);
+	char* c;
+
+	for(c=l;*c;c++)
+	{
+		if(isupper(*c))
+		{
+			*c = tolower(*c);
+		}
+	}
+
+	return 1;
+
+}
+
+int main(int argc, char* argv[])
+{
+	lower = to_lower(argv[1]);
+
+	while(*lower)
+	{
+		putchar(*(lower++));
+
+	}
+
+	puts("");
+
+	return 0;
+}
