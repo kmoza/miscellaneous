d7904901b34430d2a83351686160d5b3d9e10593
kmoza <krishoza15sep@gmail.com>
2016-12-01 17:29:28 +0530

    size test

---
+++ random_urls/sizeof_long_test.cxx
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<climits>
+
+int main(void)
+{
+	std::cout << "unsigned long : " << sizeof(unsigned long) << std::endl;
+	std::cout << "int : " << sizeof(int) << std::endl;
+	std::cout << "UMAX : " << ULONG_MAX << std::endl;
+	return 0;
+}
+
