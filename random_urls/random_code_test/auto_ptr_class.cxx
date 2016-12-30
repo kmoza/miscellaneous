b968496a398bebc614688bad2b578e810b4a0441
Krishna M Oza <koza@nvidia.com>
2016-12-30 14:34:06 +0530

    python examples

---
+++ random_urls/random_code_test/auto_ptr_class.cxx
@@ -0,0 +1,14 @@
+namespace std
+{
+	template<class Y> struct auto_ptr_ref{};
+
+	template <class X>
+	clas auto_ptr
+	{
+		public:
+			typedef X element_type;
+			explicit auto_ptr(X* p = 0) throw();
+
+
+
+
---
+++ random_urls/random_code_test/custom_allocator.cxx
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+class RequiredAllocation
+{
+	public:
+		RequiredAllocation();
+		~RequiredAllocation();
+		std::basic_string<char> s = "hello world!\n";
+};
+
+RequiredAllocation::RequiredAllocation()
+{
+	cout << "Constructor" << endl;
+}
+
+RequiredAllocation::~RequiredAllocation()
+{
+	cout << "Destructor" << endl;
+}
+
+void alloc(__gnu_cxx ::new_allocator<RequiredAllocation>* all, unsigned int size, void* pt, RequiredAllocation* t)
+{
+	try
+	{
+		all->allocate(size, pt);
+		cout << all->max_size() << endl;
+		for(auto& e : t->s)
+		{
+			cout << e;
+		}
+	}
+	catch(std::bad_alloc& e)
+	{
+		cout << e.what() << endl;
+	}
+}
+
+int main()
+{
+	__gnu_cxx ::new_allocator<RequiredAllocation>* all = new __gnu_cxx ::new_allocator<RequiredAllocation>();
+
+	RequiredAllocation t;
+	void* pt = &t;
+
+	unsigned int size = 1073741824;
+	alloc(all,size,&pt,&t);
+
+	size = 1;
+	alloc(all, size, &pt, &t);
+
+	return 0;
+}
