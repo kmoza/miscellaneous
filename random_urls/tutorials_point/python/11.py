dd72b6506a50d5416c0d294f64077f665321e340
Krishna M Oza <koza@nvidia.com>
2017-01-02 10:38:08 +0530

    key arguments in python

---
+++ random_urls/tutorials_point/python/11.py
@@ -0,0 +1,7 @@
+def printme(str):
+    'this prints a string'
+    print(str)
+    return
+
+'now print the string'
+printme(str="My String")
---
+++ random_urls/tutorials_point/python/12.py
@@ -0,0 +1,7 @@
+def printinfo(name, age):
+    'prints info'
+    print 'Name:', name
+    print 'age:', age
+    return
+
+printinfo(age=50, name='miki')
