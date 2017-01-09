445e0b5491a0cb976ee2b54c8d0c3f4eb475412c
kmoza <krishoza15sep@gmail.com>
2017-01-09 13:48:33 +0530

    operator overloading

---
+++ random_urls/tutorials_point/python/29.py
@@ -0,0 +1,26 @@
+class Parent:
+    parentAtt = 100
+    def __init__(self):
+        print 'calling parent constructor'
+
+    def parentMethod(self):
+        print 'calling parent method'
+
+    def setAttr(self, attr):
+        Parent.parentAttr = attr
+
+    def getAttr(self):
+        print 'Parent attribute : ' , Parent.parentAttr
+
+class Child(Parent):
+    def __init__(self):
+        print 'calling child constructor'
+
+    def childMethod(self):
+        print 'calling child method'
+
+c = Child()
+c.childMethod()
+c.parentMethod()
+c.setAttr(200)
+c.getAttr()
