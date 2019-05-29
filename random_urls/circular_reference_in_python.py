03cee81c33f8ca5fde15f04b5589e505d22abbbe
Krishna Oza <koza@nvidia.com>
2019-05-29 18:40:24 +0530

    cyclic reference in python checkind dtor calling sequence

---
+++ random_urls/circular_reference_in_python.py
@@ -0,0 +1,19 @@
+class FooType(object):
+	def __init__(self, id, parent):
+		self.id = id
+		self.parent = parent
+		print("Foo {} born".format(self.id))
+
+	def __del__(self):
+		print("Foo {} died".format(self.id))
+
+class BarType(object):
+	def __init__(self, id):
+		self.id = id
+		self.foo = FooType(id, self)
+		print("Bar {} born".format(self.id))
+
+	def __del__(self):
+		print("Bar {} died".format(self.id))
+
+b = BarType(12)
